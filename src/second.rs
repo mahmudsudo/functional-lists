b20e8eefea0d77ed9e2ebab205376e65c053e1d5
mahmud bello <blinkztyler@gmail.com>
2022-09-22 23:53:22 +0100

    ok stack list with options

---
+++ src/second.rs
@@ -0,0 +1,50 @@
+use std::mem;
+
+pub struct List<T> {
+    head: Link<T>,
+}
+
+// yay type aliases!
+type Link<T> = Option<Box<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        Self { head: None }
+    }
+
+    pub fn push(&mut self, elem: T) {
+        self.head = Some(Box::new(Node {
+            elem,
+            next: mem::replace(&mut self.head, None),
+        })
+        );
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|node| {
+            self.head = node.next;
+            node.elem
+        })
+    }
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| {
+            &node.elem
+        })
+    }
+
+}
+
+
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        let mut cur_link = mem::replace(&mut self.head, None);
+        while let Some(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, None);
+        }
+    }
+}
